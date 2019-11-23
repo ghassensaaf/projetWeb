03303de9530f7e6e680fd94f1df05f23d6453321
Ghassen Saaf <ghassen.saaf@esprit.tn>
2019-11-23 15:52:59 +0100

    correct 2

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start ();
-session_unset ($_SESSION["role"]);
+unset ($_SESSION["role"]);
 
 header ('location:../index.php');
 ?>
--- new_user.php
+++ new_user.php
@@ -2,7 +2,7 @@
 include 'inc/user.php';
 include 'inc/fonctionC.php';
 
-if (isset($_POST['role']) and isset($_POST['luser']) and isset($_POST['name']) and isset($_POST['email']) and isset($_POST['lpass']) and isset($_POST['phone']))
+if (isset($_POST['luser']) and isset($_POST['name']) and isset($_POST['email']) and isset($_POST['lpass']) and isset($_POST['phone']))
 {
     $user=new user($_POST['luser'],$_POST['name'],$_POST['email'],$_POST['lpass'],$_POST['phone']);
     $fonctionC= new fonctionC();
