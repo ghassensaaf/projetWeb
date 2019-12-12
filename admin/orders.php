d40acda41dae335198f150e11803bade43da14b1
Saaf Ghassen <ghassen.saaf@esprit.tn>
2019-12-12 15:19:08 +0100

    dd

--- admin/orders.php
+++ admin/orders.php
@@ -204,22 +204,5 @@ $la=$f->getOrders();
     </div><!-- .animated -->
 </div><!-- .content -->
 
-<div class="clearfix"></div>
 
 <?php  include 'inc/footer.php'?>
-
-</div><!-- /#right-panel -->
-
-<!-- Right Panel -->
-
-<!-- Scripts -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-<script src="assets/js/main.js"></script>
-
-
-</body>
-</html>
-
